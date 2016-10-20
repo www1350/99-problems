a1b35558fa312bcca4370b9499066a33945d40b5
www1350 <www_1350@163.com>
2016-10-20 14:14:24 +0800

    @add 9-12

---
+++ src/main/java/com/absurd/np/P09.java
@@ -0,0 +1,50 @@
+package com.absurd.np;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @author <a href="mailto:www_1350@163.com">Absurd</a>
+ * @version V1.0
+ * @Title: 99-problems
+ * @Package com.absurd.np
+ * @Description:
+ * Pack consecutive duplicates of list elements into sublists
+ * If a list contains repeated elements they should be placed in separate sublists.
+ * @date 2016/10/20 11:31
+ */
+public class P09 {
+
+    public static <T> List<List<T>> pack(List<T> lists) {
+        if (lists == null) {
+            throw new IllegalArgumentException("list can't be null");
+        }
+        int size = lists.size();
+        List<List<T>> packedList = new ArrayList<>();
+        if(size == 1){
+            packedList.add(lists);
+            return packedList;
+        }
+        T temp  = null;
+        List<T> tempList = new ArrayList<>();
+        for(int i=0;i<size;i++){
+            T e = lists.get(i);
+            if(!e.equals(temp)){
+                tempList = new ArrayList<>();
+                packedList.add(tempList);
+            }
+            tempList.add(e);
+            temp = e;
+
+        }
+        return packedList;
+    }
+
+//    public static <T> List<List<T>> pack_stream(List<T> lists) {
+//        if (lists == null) {
+//            throw new IllegalArgumentException("list can't be null");
+//        }
+//        lists.stream().collect(Collectors.groupBy(String::length, Collectors.toList()));
+//    }
+}
---
+++ src/main/java/com/absurd/np/P10.java
@@ -0,0 +1,41 @@
+package com.absurd.np;
+
+import java.util.AbstractMap.SimpleEntry;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @author <a href="mailto:www_1350@163.com">Absurd</a>
+ * @version V1.0
+ * @Title: 99-problems
+ * @Package com.absurd.np
+ * @Description:
+ *  Run-length encoding of a list
+ *  Use the result of problem 1.09 to implement the so-called run-length encoding data compression method. Consecutive duplicates of elements are encoded as terms [N,E] where N is the number of duplicates of the element E.
+ * @date 2016/10/20 13:34
+ */
+public class P10 {
+    public static <T> List<SimpleEntry<Integer,T>> encode(List<T> lists) {
+        List<List<T>> listAll = P09.pack(lists);
+        List<SimpleEntry<Integer,T>> result = new ArrayList<>();
+                listAll.forEach(
+                        e->{
+                            SimpleEntry<Integer,T> se = new SimpleEntry<Integer, T>(e.size(),e.get(0));
+                            result.add(se);
+                        }
+
+                );
+        return result;
+    }
+
+
+
+    public static <T> List<SimpleEntry<Integer,T>> encode_stream(List<T> lists) {
+        return  P09.pack(lists)
+                .stream()
+                .map(e -> new SimpleEntry<>(e.size(),e.get(0)) )
+                .collect(Collectors.toList());
+
+    }
+}
